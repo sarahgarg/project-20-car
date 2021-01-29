98bf2a61c070389f7c6c7b6b29859a0ac093338c
sarahgarg <77622091+sarahgarg@users.noreply.github.com>
2021-01-29 19:06:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-20-car
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+
+var speed,weight;
+var wall,car;
+function setup() {
+  createCanvas(1600,400);
+  wall= createSprite(1500, 200, 40, height/2);
+ car = createSprite(50,200,50,50);
+ wall.shapeColor=color(80,80,80);
+ 
+ 
+ speed=random(55,90);
+ weight=random(400,1500);
+}
+
+function draw() {
+  background("black");
+  car.velocityX=speed;
+ if (wall.x-car.x < car.width/2+wall.width/2)
+ {
+  var deformation=0.5*weight*speed*speed/22500;
+   car.velocityX=0;
+   if(deformation<100)
+   {
+car.shapecolor=color(0,255,0);
+   }
+   else if (deformation>=100 && deformation>=180)
+   {
+     car.shapeColor=color(230,230,0);
+   }
+   else
+   {
+     car.shapeColor=color(255,0,0)
+   }
+ }
+  drawSprites();
+}
